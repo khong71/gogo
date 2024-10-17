0a3092ce2fa6e3164f53c8b9be1fb577a1c75db9
khong71 <khongsak.sutthithang@gmail.com>
2024-10-17 21:02:38 +0700

    ..

--- handler/user.handler.go
+++ handler/user.handler.go
@@ -7,6 +7,34 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Register
+
+func Register(ctx *fiber.Ctx) error {
+	var Register entity.Register
+
+	// รับข้อมูลจาก request body
+	if err := ctx.BodyParser(&Register); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "ไม่สามารถรับข้อมูลได้",
+		})
+	}
+
+	// บันทึกข้อมูลผู้ใช้ใหม่ลงในตาราง User
+	if result := database.MYSQL.Debug().Table("User").Create(&Register); result.Error != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "ไม่สามารถเพิ่มข้อมูลได้",
+		})
+	}
+
+	// ส่งข้อมูลผู้ใช้ที่ถูกเพิ่มกลับไป
+	return ctx.JSON(fiber.Map{
+		"message": "เพิ่มผู้ใช้สำเร็จ",
+	})
+}
+
+
+//-------------------------------------------------------------------------------------------------------
+
 func GetUsers(ctx *fiber.Ctx) error {
 	var user []entity.User
 
@@ -24,29 +52,6 @@ func GetUser_id(ctx *fiber.Ctx) error {
 	ctx.JSON(user)
 
 	return ctx.JSON(user)
-}
-
-func PostUser(ctx *fiber.Ctx) error {
-	var PostUser entity.PostUser
-
-	// รับข้อมูลจาก request body
-	if err := ctx.BodyParser(&PostUser); err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "ไม่สามารถรับข้อมูลได้",
-		})
-	}
-
-	// บันทึกข้อมูลผู้ใช้ใหม่ลงในตาราง User
-	if result := database.MYSQL.Debug().Table("User").Create(&PostUser); result.Error != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "ไม่สามารถเพิ่มข้อมูลได้",
-		})
-	}
-
-	// ส่งข้อมูลผู้ใช้ที่ถูกเพิ่มกลับไป
-	return ctx.JSON(fiber.Map{
-		"message": "เพิ่มผู้ใช้สำเร็จ",
-	})
 }
 
 func DeleteUserAll(ctx *fiber.Ctx) error {
