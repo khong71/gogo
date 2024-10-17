3b7f957d77fbc08f95a755fe61a65f27a554e5e3
khong71 <khongsak.sutthithang@gmail.com>
2024-10-17 22:32:44 +0700

    ..

--- model/entity/register.go
+++ model/entity/register.go
@@ -11,11 +11,11 @@ type Register struct {
 }
 
 type RegisterDriver struct {
-	Raider_name     string `json:"raider_name"`
-	Raider_email    string `json:"raider_email"`
-	Raider_Phone    int    `json:"raider_phone"`
-	Raider_image    string `json:"raider_image"`
-	Raider_numder   string `json:"raider_numder-plate"`
+	Raider_name  string `json:"raider_name"`
+	Raider_email string `json:"raider_email"`
+	Raider_Phone int    `json:"raider_phone"`
+	Raider_image string `json:"raider_image"`
+	// Raider_numder   string `json:"raider_numder-plate"`
 	Raider_password string `json:"raider_password"`
 	Raider_location string `json:"raider_location"`
 }
