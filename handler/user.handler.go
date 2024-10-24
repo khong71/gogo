47648b7747c9eeaa331bf3ba803b0d0c3214891e
khong71 <khongsak.sutthithang@gmail.com>
2024-10-24 21:06:56 +0700

    ..

--- handler/user.handler.go
+++ handler/user.handler.go
@@ -222,7 +222,7 @@ func GetUser_id(ctx *fiber.Ctx) error {
 }
 
 func GetOrders(ctx *fiber.Ctx) error {
-	var orders []entity.InsertOrder
+	var orders []entity.GetOrder
 
 	// ควรใช้ slice เนื่องจากดึงข้อมูลหลายแถว
 	database.MYSQL.Debug().Table("Order").Find(&orders)
@@ -241,7 +241,7 @@ func GetOrdersId(ctx *fiber.Ctx) error {
 		OrderSenderID   uint    `json:"order_sender_id"`   // ID ของผู้ส่ง
 		OrderReceiverID uint    `json:"order_receiver_id"` // ID ของผู้รับ
 		UserID          uint    `json:"user_id"`           // ID ของผู้ใช้
-		User_name        string  `json:"user_name"`         // ชื่อผู้ใช้
+		User_name       string  `json:"user_name"`         // ชื่อผู้ใช้
 		User_Phone      string  `json:"user_phone"`
 		User_image      string  `json:"user_image"`
 	}
